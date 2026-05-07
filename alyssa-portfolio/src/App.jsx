6f665658341602b0e16defd3be00361b1d260063
Alyssa Mercado <amercado1199@gmail.com>
2026-05-07 15:33:59 -0400

    fix react router

--- alyssa-portfolio/src/App.jsx
+++ alyssa-portfolio/src/App.jsx
@@ -1,41 +1,26 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
-
-import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import ProjectsPage from "./pages/ProjectsPage";
-
-import "./styles/App.css";
+import "./App.css";
 
 function Home() {
 return (
-<>
+<div>
 <Hero />
-<About />
-<Skills />
-<Contact />
-</>
+</div>
 );
 }
 
 function App() {
 return (
 <HashRouter>
-<div className="app">
-<NavBar />
-
 <Routes>
 <Route path="/" element={<Home />} />
 <Route path="/projects" element={<ProjectsPage />} />
 </Routes>
-
-<Footer />
-</div>
 </HashRouter>
 );
 }
 
 export default App;
+
